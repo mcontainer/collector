93c2d0fb39572d2d469db2483bde64feb626e7ab
Paul Boutes <paul.boutes@gmail.com>
2017-08-29 01:19:54 +0200

    feat(sample): update vagrant configuration

--- sample/init.sh
+++ sample/init.sh
@@ -5,7 +5,7 @@ PREFIX_INFO="[INFO] ::"
 function fetchIP() {
     vagrant ssh $1 -c "ip addr show eth1 | grep 'inet ' | sed -e 's/^.*inet //' -e 's/\/.*$//'" 2> /dev/null
 }
-vagrant up
+#vagrant up
 
 
 echo "$PREFIX_INFO Fetching master and worker ip"
@@ -24,13 +24,21 @@ echo "$PREFIX_INFO executing -> $INIT_SWARM"
 vagrant ssh core-01 -c "$INIT_SWARM" 2> /dev/null
 
 echo "$PREFIX_INFO Fetching swarm join worker token"
-WORKER_TOKEN=`vagrant ssh core-01 -c "docker swarm join-token worker -q" 2> /dev/null`
+WORKER_TOKEN=`vagrant ssh core-01 -c "docker swarm join-token worker -q" | tr -d '[:space:]' 2> /dev/null`
+
+echo "$PREFIX_INFO TOKEN = $WORKER_TOKEN"
 
 echo "$PREFIX_INFO Adding worker node (core-02) to swarm cluster"
-vagrant ssh core-01 -c "docker swarm join --token $WORKER_TOKEN $MASTER_IP:2377"
+vagrant ssh core-02 -c "docker swarm join --token $WORKER_TOKEN $MASTER_IP:2377"
 
-echo "$PREFIX_INFO starting collector binary on master node"
-vagrant ssh core-01 -c "sudo systemctl start collector.service"
+for i in 1 2
+do
+    echo "$PREFIX_INFO starting processor binary on master node"
+    vagrant ssh "core-0$i" -c "sudo systemctl start processor.service"
+
+    echo "$PREFIX_INFO starting collector binary on master node"
+    vagrant ssh "core-0$i" -c "sudo systemctl start collector.service"
+done
 
 echo "$PREFIX_INFO starting docker services"
 vagrant ssh core-01 -c "docker network create --driver overlay yolo-network"
--- sample/provision.sh
+++ sample/provision.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 mkdir -p /opt/collector
+mkdir -p /opt/processor
 mv collector /opt/collector
+mv processor /opt/processor
 
 mv collector.service /etc/systemd/system/
+mv processor.service /etc/systemd/system/
 
+systemctl enable processor.service
 systemctl enable collector.service
